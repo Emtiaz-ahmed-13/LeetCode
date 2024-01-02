07eef28f375d07097b77e4331d7828ce0859cbe6
princeemtiaz <138528626+EMTIAZAHMED2060@users.noreply.github.com>
2024-01-02 12:31:58 +0600

    Day-02

---
+++ Day-02/442. Find All Duplicates in an Array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        set<int>st;
+        vector<int>v;
+        for(int i =0;i<nums.size();i++)
+        {
+            if(st.count(nums[i]))
+            {
+                v.push_back(nums[i]);
+            }
+            else
+            {
+                st.insert(nums[i]);
+            }
+        }
+        return v;
+    }
+};
