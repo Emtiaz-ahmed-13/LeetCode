7909a5603992e24e39a085be2ff123e998c77731
EMTIAZAHMED2060 <emtiaz.ahmed@g.bracu.ac.bd>
2024-01-14 23:38:54 +0600

    Day-9

---
+++ Day-09/872.LeafSimilarTrees.py
@@ -0,0 +1,15 @@
+
+
+# problem Link:
+#     https://leetcode.com/problems/leaf-similar-trees/
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(root):
+          
+            if root is None:
+                return []
+            leaves = dfs(root.left) + dfs(root.right)
+            return leaves or [root.val]
+        return dfs(root1) == dfs(root2)
+        
