65348734e9741b658b6f279b55ff49ee7216c349
EMTIAZAHMED2060 <emtiaz.ahmed@g.bracu.ac.bd>
2024-01-19 19:49:07 +0600

    day-12 fix

--- Day-12/144.BinaryTreePreOrderTraversal.py
+++ Day-12/144.BinaryTreePreOrderTraversal.py
@@ -2,6 +2,10 @@
 # Problem Link:
 #     https://leetcode.com/problems/binary-tree-preorder-traversal/description/
 
+from ast import List
+from typing import Optional
+
+
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
