89a46d280feb8c9e149d95bedb2ebd1b64353899
EMTIAZAHMED2060 <emtiaz.ahmed@g.bracu.ac.bd>
2024-01-19 19:52:38 +0600

    day-16 photo added

--- Day-16/1657.DetermineIfTwoStringsAreClose.py
+++ Day-16/1657.DetermineIfTwoStringsAreClose.py
@@ -1,6 +1,9 @@
 # Problem Link:
 #     https://leetcode.com/problems/determine-if-two-strings-are-close/
 
+from collections import Counter
+
+
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
 
