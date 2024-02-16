ca2b2d5b2ab92b82316256cc4cd5f69cbfbfa40b
EMTIAZAHMED2060 <emtiaz.ahmed@g.bracu.ac.bd>
2024-02-17 00:08:11 +0600

    Day-40 added

---
+++ Day-40/279. Perfect Squares.py
@@ -0,0 +1,12 @@
+#PROBELM LINK:
+    #https://leetcode.com/problems/perfect-squares/description/?envType=daily-question&envId=2024-02-08
+    
+class Solution:
+    def numSquares(self, n: int) -> int:
+        if n==0:
+            return 0
+        if n<0:
+            return float("inf")
+        mini=n
+        while(i*j<=n):
+            mini=min
